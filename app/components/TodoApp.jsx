950129e267755c14fa53a4f890d605573213b00b
Victor <papadoginc@gmail.com>
2016-12-04 19:14:42 +0300

    Add toggle

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -13,16 +13,20 @@ class TodoApp extends React.Component {
             todos: [
                 {
                     id: uuid(),
-                    text: 'Wak the dog'
+                    text: 'Wak the dog',
+                    completed: false
                 }, {
                     id: uuid(),
-                    text: 'Clean the yard'
+                    text: 'Clean the yard',
+                    completed: false
                 }, {
                     id: uuid(),
-                    text: 'Close the door'
+                    text: 'Close the door',
+                    completed: true
                 }, {
                     id: uuid(),
-                    text: 'Wash the pants'
+                    text: 'Wash the pants',
+                    completed: false
                 }
             ]
         }
@@ -40,17 +44,28 @@ class TodoApp extends React.Component {
             ...this.state.todos,
             {
               id: uuid(),
-              text:text
+              text:text,
+              completed: false
             }
           ]
         });
+    }
+    handleToggle(id){
+      let updatedTodos = this.state.todos.map((todo)=>{
+        if(todo.id === id){
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+
+      this.setState({todos:updatedTodos});
     }
     render() {
         let {todos} = this.state;
         return (
             <div>
                 <TodoSearch onSearch={this.handleOnSearch.bind(this)}/>
-                <TodoList todos={todos}/>
+                <TodoList todos={todos} onToggle={this.handleToggle.bind(this)}/>
                 <AddTodo onAddTodo={this.handleAddTodo.bind(this)}/>
             </div>
           );
